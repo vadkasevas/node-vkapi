85d06b96af011e71227e54c65b973241be5ae48f
olnaz <olnazx@gmail.com>
2017-10-05 22:28:39 +0300

    Update captcha recognizer

--- src/captcha-recognizer.js
+++ src/captcha-recognizer.js
@@ -1,83 +1,122 @@
 'use strict';
 
 /**
- * Recognizing VK Captcha via anti-captcha.com (or rucaptcha.com/antigate.com) service
+ * CaptchaRecognizer is used to recognize VK captcha via one of supported services.
+ *
+ * Supported services:
+ *   anti-captcha
+ *   antigate
+ *   rucaptcha
  */
 
-const prequest = require('request-promise');
+/**
+ * Module dependencies.
+ * @private
+ */
+const { URLSearchParams } = require('url');
+const fetch               = require('./lib/fetch');
 
 class CaptchaRecognizer {
-  constructor (params) {
-    this.key = params.key;
+  /**
+   * Constructor.
+   * @param  {String} service Service name
+   * @param  {String} apiKey  Service API key
+   * @return {this}
+   */
+  constructor (service, apiKey) {
+    /**
+     * Service API key.
+     * @type {String}
+     */
+    this.apiKey = apiKey;
 
-    this.uploadUrl = 'http://' + params.service + '.com/in.php';
-    this.checkUrl = 'http://' + params.service + '.com/res.php';
+    /**
+     * Urls to upload captcha and check
+     * its recognizing completion.
+     * @type {String}
+     */
+    this.checkUrl  = `http://${service}.com/res.php`;
+    this.uploadUrl = `http://${service}.com/in.php`;
 
-    this.waitTimes = 0;
+    /**
+     * How many times we have waited for captcha recognizing.
+     * @type {Number}
+     */
+    this.waitsCount = 0;
   }
 
+  /**
+   * Recognizes a captcha image by an URL.
+   * @param  {String}            url Image URL
+   * @return {Promise => String}     Captcha key recognized
+   * @public
+   */
   recognize (url) {
-    return this.download(url)
-      .then(buf => this.convert(buf))
-      .then(base64str => this.upload(base64str))
-      .then(id => this.wait(id))
-      .then(id => this.check(id));
-  }
-
-  download (url) {
-    return prequest(url, { encoding: null });
-  }
-
-  convert (buffer) {
-    return buffer.toString('base64');
-  }
-
-  upload (base64str) {
-    return prequest.post(this.uploadUrl, {
-      formData: {
-        method: 'base64', 
-        key: this.key, 
-        body: {
-          value: base64str, 
-          options: {
-            contentType: 'image/jpg'
-          }
+    return fetch(url)
+      .then(image => image.buffer())
+      .then(imageBuffer => imageBuffer.toString('base64'))
+      .then(imageBase64 => fetch(this.uploadUrl, {
+        body:   new URLSearchParams({
+          body:   imageBase64,
+          key:    this.apiKey,
+          method: 'base64'
+        }),
+        method: 'POST'
+      }))
+      .then(response => response.text())
+      .then(response => {
+        if (response.startsWith('OK')) {
+          return response.slice(3);
         }
-      }
-    }).then(res => {
-      if (res.startsWith('OK')) 
-        return res.split('|')[1];
 
-      throw res;
-    });
+        throw response;
+      })
+      .then(captchaId => this._wait(captchaId))
+      .then(captchaId => this._check(captchaId));
   }
 
-  wait (captchaId, check) {
-    return new Promise(resolve => {
-      setTimeout(() => {
-        this.waitTimes++;
-
-        return resolve(check ? this.check(captchaId) : captchaId);
-      }, this.waitTimes === 0 ? 10000 : 5000);
-    });
-  }
-
-  check (captchaId) {
-    return prequest(this.checkUrl, {
+  /**
+   * Checks for completion of recognizing.
+   * @param   {String}            captchaId Captcha ID
+   * @return  {Promise => String}           Captcha key recognized
+   * @private
+   */
+  _check (captchaId) {
+    return fetch(this.checkUrl, {
       qs: {
-        key: this.key, 
-        action: 'get', 
-        id: captchaId
+        action: 'get',
+        id:     captchaId,
+        key:    this.apiKey
       }
-    }).then(res => {
-      if (res.startsWith('ERROR')) 
-        throw res;
+    })
+      .then(response => response.text())
+      .then(response => {
+        if (response.startsWith('ERROR')) {
+          throw response;
+        }
 
-      if (~res.indexOf('NOT_READY')) 
-        return this.wait(captchaId, true);
+        if (~response.indexOf('NOT_READY')) {
+          return this._wait(captchaId, true);
+        }
 
-      return res.split('|')[1];
-    });
+        return response.split('|')[1];
+      });
+  }
+
+  /**
+   * Waits until captcha gets recognized.
+   * @param   {String}  captchaId    Captcha ID
+   * @param   {Boolean} fromChecking Is this function was called from "check"-function?
+   * @return  {Promise}
+   * @private
+   */
+  _wait (captchaId, fromChecking) {
+    return new Promise(resolve => setTimeout(() => resolve(), this.waitsCount === 0 ? 10000 : 5000))
+      .then(() => {
+        this.waitsCount++;
+
+        return fromChecking ? this._check(captchaId) : captchaId;
+      });
   }
 }
 
